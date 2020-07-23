c89f20f6e7f4b2348597e42e4f7580d2d06f7164
hughesrva <hughesrva@gmail.com>
2020-07-23 09:34:30 -0400

    adding more practice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServerComponent } from './servers/server/server.component';
-import { ServersComponent } from './servers/servers.component'
+import { ServersComponent } from './servers/servers.component';
+import { WarningAlertComponent } from './warning-alert/warning-alert.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component'
 
 @NgModule({
-  declarations: [AppComponent, ServerComponent, ServersComponent],
+  declarations: [AppComponent, ServerComponent, ServersComponent, WarningAlertComponent, SuccessAlertComponent],
   imports: [FormsModule, BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,9 +1,25 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'app-server',
-    templateUrl: './server.component.html',
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css'],
 })
 export class ServerComponent {
+  serverID: number;
+  serverStatus: string;
 
+  getServerID() {
+    this.serverID = Math.floor(Math.random() * 21);
+    return this.serverID;
+  }
+
+  getServerStatus() {
+    if (this.serverID > 10) {
+      this.serverStatus = 'offline';
+    } else {
+      this.serverStatus = 'online';
+    }
+    return this.serverStatus;
+  }
 }
